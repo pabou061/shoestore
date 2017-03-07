daced4c66be77264cd49d4fd5ba0c49dff8599f3
patrick <patrick.as@live.com>
2017-03-07 16:50:52 -0500

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,16 @@
 Rails.application.routes.draw do
 
-	
-
 	root "pages#home"
 	get "/home" => "pages#home"
 	get "/location" => "pages#location"
 	get	"/cart" => "pages#cart"
 	get "/signup" => "pages#signup"
-	get "/login" => "pages#login"
+	
+	resources :logins, only: [:index] 
 
 	resources :categories, only: [:show, :index] do
 		resources :subcategories do
 			resources :shoes
 		end
 	end
-
-	# get "/category/[:category_id]/subcategory/[:subcategory_id]/shoes/[:id]"
-
-
 end
