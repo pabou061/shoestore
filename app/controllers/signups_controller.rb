99fd476baa9f1317bc576b7dafdcad51f5200a95
patrick <patrick.as@live.com>
2017-03-17 10:27:33 -0400

    update signup functions

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -2,22 +2,34 @@ class SignupsController < ApplicationController
 
 
 	def new
+		@signup = Signup.new
+		if params[:message]
+			@message = 
+			params[:message]
+		else
+			@message="test"
+		end
   	end
 
     def create
-    	user = Signup.new(user_params)
-	    if user.save
-	      session[:user_id] = user.id
-	      redirect_to '/'
-	    else
-	      redirect_to '/'
-	    end
+    	@signup = Signup.new(user_params)
+    	if user_params[:password_confirmation]=user_params[:password]
+		    if @signup.save
+		      session[:user_id] = @signup.id
+		      redirect_to '/'
+		    else
+		      render new_signup_path
+		    end
+		else 
+			@message= "Password do not match"
+			 render new_signup_path(:message)
+		end
     end
 
 private
 
     def user_params
-    params.require(:login).permit(:fname,:lname,:gender, :email, :password, :password_confirmation)
+    params.require(:signup).permit(:fname,:lname,:gender,:dob, :email, :password, :password_confirmation)
   end
 
 
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -2,7 +2,9 @@
 class Signup < ActiveRecord::Base
 
 has_secure_password
-
 self.table_name = "shoestore.client"
+
+
+
 
 end
