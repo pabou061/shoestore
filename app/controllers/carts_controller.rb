5a44a442cdad2c3627f6809258dcaf23c5644570
patrick <patrick.as@live.com>
2017-03-21 16:16:41 -0400

    cart controller

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,31 @@
+class CartsController < ApplicationController
+
+
+
+def index
+   @cart = Cart.find_by cid: session[:user_id]
+end
+
+
+def create
+
+
+  if session[:user_id]
+      cart= Cart.new(cart_params) 
+      if cart.save
+        flash[:notice] = "Successfully added to your cart"
+
+      else
+        flash[:error] = "Please Make sure you checked everything"
+      end 
+  else 
+      flash[:error] = "You have to login to add to cart"
+  end
+  redirect_to :back
+end
+
+def cart_params
+    params.require(:cart).permit(:sid,:cid,:sizeid,:colorid,:quantity,:flag)
+end
+
+end
