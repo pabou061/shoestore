e5881b6e2a3a55c458b74181ed2c5adcdc0590bb
patrick <patrick.as@live.com>
2017-03-21 16:15:31 -0400

    fixed routing for cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 	root "pages#home"
 	get "/home" => "pages#home"
 	get "/location" => "pages#location"
-	get	"/cart" => "pages#cart"
 	
 	resources :logins, only: [:new, :create, :destroy]
 	resources :signups, only: [:new,:create]
+	resources :carts, only: [:index,:create]
 
 	resources :categories, only: [:show, :index] do
 		resources :subcategories do
