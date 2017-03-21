f4cd20b2b3227f05d7a6392f88bb737074f1cfd5
patrick <patrick.as@live.com>
2017-03-21 16:18:14 -0400

    save shoes to the database

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,11 +1,5 @@
 class ShoesController < ApplicationController
 
-
-
-
-
-
-
 	def index
 		@category= params[:category_id]
 		@subcategory= params[:subcategory_id]
@@ -15,7 +9,6 @@ class ShoesController < ApplicationController
 			Where s.sid=a.sid and quantity>0 and s.catid=?
 			group by s.sid", @subcategory]
 
-		render("index")
 
 	end
 
@@ -23,19 +16,30 @@ class ShoesController < ApplicationController
 		@shoe = Shoe.find params[:id]
 
 		@available = 
-		Availablesize.find_by_sql ["SELECT s.size,a.quantity
+		Availablesize.find_by_sql ["SELECT *
 			FROM shoestore.availablesizes a,shoestore.sizes s
 			Where a.sizeid = s.sizeid and a.sid= ? ", @shoe.sid]
 
 		@availablecolor = 
-		Availablecolor.find_by_sql ["SELECT c.color
+		Availablecolor.find_by_sql ["SELECT *
 			FROM shoestore.availablecolors a, shoestore.colors c
 			Where a.colorid = c.colorid and a.sid= ? ", @shoe.sid]
 
 
 		@shoe = Shoe.find params[:id]
-		render("show")
+		@cart= Cart.new
 		
 	end
+
+	def create
+
+		redirect_to category_subcategory_shoes_path
+		
+	end
+
+	def cart_params
+    params.require(:size,:quantity)
+  end
+
 
 end
