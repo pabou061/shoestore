05591ed1eb9814b6a16d83937c30d572ed1f598e
patrick <patrick.as@live.com>
2017-03-23 15:16:22 -0400

    fixed empty cart issue

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,30 +2,34 @@ class CartsController < ApplicationController
 
 
 
-def index
-   @cart = Cart.where(cid: session[:user_id])
-end
-
-
-def create
-
-
-  if session[:user_id]
-      cart= Cart.new(cart_params) 
-      if cart.save
-        flash[:notice] = "Successfully added to your cart"
-
-      else
-        flash[:error] = "Please Make sure you checked everything"
-      end 
-  else 
-      flash[:error] = "You have to login to add to cart"
+  def index
+     @cart = Cart.where(cid: session[:user_id])
   end
-  redirect_to :back
-end
 
-def cart_params
-    params.require(:cart).permit(:sid,:cid,:sizeid,:colorid,:quantity,:flag)
-end
+
+  def create
+    if session[:user_id]
+        cart= Cart.new(cart_params) 
+        if cart.save
+          flash[:notice] = "Successfully added to your cart"
+
+        else
+          flash[:error] = "Please Make sure you checked everything"
+        end 
+    else 
+        flash[:error] = "You have to login to add to cart"
+    end
+    redirect_to :back
+  end
+
+  def cart_params
+      params.require(:cart).permit(:sid,:cid,:sizeid,:colorid,:quantity,:flag)
+  end
+
+  def destroy
+    cart=Cart.find params[:id]
+    cart.destroy
+    redirect_to carts_path
+  end
 
 end
