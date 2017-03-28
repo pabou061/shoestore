3d2f1f5b8192c8d1b52cf47b4f3da61e3fbaa9f3
patrick <patrick.as@live.com>
2017-03-28 16:22:15 -0400

    initiale mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,18 @@
 # Load the Rails application.
 require_relative 'application'
 
+
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'your_sendgrid_username',
+  :password => 'your_sendgrid_password',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
