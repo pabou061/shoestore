647d73964f113466ac6fc167770ceafcfc8fa594
patrick <patrick.as@live.com>
2017-03-29 19:26:05 -0400

    remove sendgrid

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,6 @@
 class CartsController < ApplicationController
 
-    require 'sendgrid-ruby'
-    include SendGrid
+ 
 
   def index
      @cart = Cart.where(cid: session[:user_id], flag:1)
@@ -48,7 +47,7 @@ class CartsController < ApplicationController
   def checkout
 
     cart = Cart.where(cid: session[:user_id], flag:1)
-    # send_email
+ 
     cart.each do |c|
       c.update flag: 2
     end 
@@ -58,20 +57,6 @@ class CartsController < ApplicationController
   end
 
 
-#   def send_email
 
-
-# from = Email.new(email: 'the-shoe-store@example.com')
-# to = Email.new(email: 'pabou061@uottawa.ca')
-# subject = 'Sending with SendGrid is Fun'
-# content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-# mail = Mail.new(from, subject, to, content)
-
-# sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
-# response = sg.client.mail._('send').post(request_body: mail.to_json)
-# puts response.status_code
-# puts response.body
-# puts response.headers
-#   end
 
 end
