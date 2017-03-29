91d86ebfa1c9da161a38ed34b4d1952e0b1266ae
patrick <patrick.as@live.com>
2017-03-29 18:21:01 -0400

    revert some changes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -40,7 +40,6 @@ class CartsController < ApplicationController
   end
 
   def destroy
-    #a
     cart=Cart.find params[:id]
     cart.delete
     redirect_to carts_path
@@ -49,7 +48,7 @@ class CartsController < ApplicationController
   def checkout
 
     cart = Cart.where(cid: session[:user_id], flag:1)
-
+    # send_email
     cart.each do |c|
       c.update flag: 2
     end 
@@ -59,6 +58,20 @@ class CartsController < ApplicationController
   end
 
 
+#   def send_email
 
+
+# from = Email.new(email: 'the-shoe-store@example.com')
+# to = Email.new(email: 'pabou061@uottawa.ca')
+# subject = 'Sending with SendGrid is Fun'
+# content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+# mail = Mail.new(from, subject, to, content)
+
+# sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+# response = sg.client.mail._('send').post(request_body: mail.to_json)
+# puts response.status_code
+# puts response.body
+# puts response.headers
+#   end
 
 end
